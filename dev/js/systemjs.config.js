2f521e08388ccec90ca5b33e6251414c309f00cf
Zafer <zafer.qadi@gmail.com>
2016-06-19 02:15:18 +0300

    deleted

--- dev/js/systemjs.config.js
+++ dev/js/systemjs.config.js
@@ -11,10 +11,10 @@
     //'dev':                        'dev',
     //'prod':                       'prod',
     //'@angular':                   'node_modules/@angular',
-    '@angular':                   'libs/@angular',
+    '@angular':                   '../node_modules/@angular',
     // 'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     // 'rxjs':                       'node_modules/rxjs',
-    'rxjs':                       'libs/rxjs'
+    'rxjs':                       '../node_modules/rxjs'
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
